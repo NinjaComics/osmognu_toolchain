28dd6cb33c0484e19b06a1fa051ebf40180ab4a6
Ravi Sharan <ravisharan@iith.ac.in>
2014-12-08 13:07:08 +0530

    Add install script and final toolchain

---
+++ install/lib/gcc/arm-elf/4.6.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/ravi/Desktop/osmo_gnutc/src/gcc-4.6.4/configure --target=arm-elf --prefix=/home/ravi/Desktop/osmo_gnutc/install --enable-interwork --enable-multilib --with-float=soft --disable-werror --enable-languages=c,c++ --with-newlib --with-headers=/home/ravi/Desktop/osmo_gnutc/src/newlib-2.1.0/newlib/libc/include --with-system-zlib --disable-shared";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "soft" } };
